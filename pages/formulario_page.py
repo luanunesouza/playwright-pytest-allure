870e7772007d5a01066b8e60b2ab01f5c6827cda
luanunesouza <luanunesouza@hotmail.com>
2026-02-05 10:50:16 -0300

    Correções na Pipeline

--- pages/formulario_page.py
+++ pages/formulario_page.py
@@ -5,40 +5,33 @@ class FormularioPage:
 
     def __init__(self, page):
         self.page = page
+        self.frame = page.frame_locator('iframe[src*="rdstation"]')
 
     @allure.step("Acessar página de inscrição")
     def acessar(self):
-        self.page.goto("https://www.hashtagtreinamentos.com/curso-python")
+        self.page.goto(
+            "https://www.hashtagtreinamentos.com/curso-python",
+            wait_until="networkidle"
+        )
 
     @allure.step("Preencher nome: {nome}")
     def preencher_nome(self, nome):
-        frame = self.page.frame_locator("iframe")
-        frame.locator("#firstname").fill(nome)
-
-#    def preencher_nome(self, nome):
-#        self.page.fill('xpath=//*[@id="firstname"]', nome)
+        self.frame.locator("#firstname").fill(nome)
 
     @allure.step("Preencher email: {email}")
     def preencher_email(self, email):
-        frame = self.page.frame_locator("iframe")
-        frame.locator("#email").fill(email)
+        self.frame.locator("#email").fill(email)
 
     @allure.step("Preencher telefone: {telefone}")
     def preencher_telefone(self, telefone):
-        frame = self.page.frame_locator("iframe")
-        frame.locator("#phone").fill(telefone)
+        self.frame.locator("#phone").fill(telefone)
 
     @allure.step("Enviar formulário")
     def enviar(self):
-        frame = self.page.frame_locator("iframe")
-        frame.locator("#_form_2475_submit").click()
+        self.frame.locator("#_form_2475_submit").click()
 
     @allure.step("Validar inscrição com sucesso")
     def validar_sucesso(self):
-        # Aguarda a navegação finalizar
-        self.page.wait_for_load_state("networkidle")
-
-        # Aguarda o texto correto (SEM o "1")
         expect(
             self.page.get_by_text("Conheça o Programa Completo")
-        ).to_be_visible(timeout=15_000)
+        ).to_be_visible(timeout=15000)
